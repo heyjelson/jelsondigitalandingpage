f3ab278fcae73f06e68dee618660592dbd1bd921
Jelson <heyjelson@gmail.com>
2022-03-15 17:29:01 +0800

    Contact form is active

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+window.onload = function() {
+            document.getElementById('form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                
+                this.user_number.value = Math.random() * 100000 | 0;
+                this.user_name.value;
+                this.user_email.value;
+                this.message.value;
 
+                this.user_name.value = "";
+                this.user_email.value = "";
+                this.message.value = "";
+                
+
+                emailjs.sendForm('contact_service', 'contact_form', this)
+                    .then(function() {
+                        console.log('SUCCESS!');
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
 
